1a06fc7338b75dbb57487c4ad7f74385b2fa2668
Erika <88861951+ErikaRoguie@users.noreply.github.com>
2022-03-13 14:00:53 -0700

    full backhalf of app

--- routes/Index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.use(require('./apiRoutes/departmentRoutes'));
-router.use(require('./apiRoutes/employesRoutes'));
-
-
-
-module.exports = router;
